21f1b9558de511376bdc2a9a5bc7fb7b1813d8c7
0u812 <0u812@users.noreply.github.com>
2017-05-03 08:51:52 -0700

    fix phrasedml script

--- run-phrased.py
+++ run-phrased.py
@@ -3,15 +3,21 @@ import phrasedml, os
 with open('./model/BIOMD0000000144.xml') as f:
     sbml_str = f.read()
 
-with open('./phrased/Calzone2007-simulation-figure-1B.xml') as f:
-    phrased_str = f.read()
+with open('./experiment/Calzone2007-simulation-figure-1B.xml') as f:
+    sedml_in = f.read()
 
 phrasedml.setReferencedSBML('../model/BIOMD0000000144.xml', sbml_str)
 
+phrasedml.convertString(sedml_in)
+
+phrased_str = phrasedml.getLastPhraSEDML()
+
 phrasedml.convertString(phrased_str)
 
-sedml_str = phrasedml.getLastSEDML()
+sedml_out = phrasedml.getLastSEDML()
 
-os.makedirs('phrased')
+if not os.path.exists('phrased'):
+    os.makedirs('phrased')
+
 with open('./phrased/Calzone2007-simulation-figure-1B.xml', 'w') as f:
-    f.write(sedml_str)
+    f.write(sedml_out)
