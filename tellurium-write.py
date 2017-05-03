419b0ab014851fc44d953bfaa9755c47f494bb28
0u812 <0u812@users.noreply.github.com>
2017-05-03 09:41:11 -0700

    activate phrasedml ver

--- tellurium-write.py
+++ tellurium-write.py
@@ -1,9 +1,9 @@
 import tellurium as te
 
 # works
-inline_omex = te.convertCombineArchive('out_sedml.omex')
+# inline_omex = te.convertCombineArchive('out_sedml.omex')
 # doesn't work
-# inline_omex = te.convertCombineArchive('out_phrasedml.omex')
+inline_omex = te.convertCombineArchive('out_phrasedml.omex')
 print('inline omex:')
 print(inline_omex)
 te.exportInlineOmex(inline_omex, 'out_tellurium.omex')
