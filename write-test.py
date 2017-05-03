123af529189540a663e9a2d0b9d45401ab84e624
0u812 <0u812@users.noreply.github.com>
2017-05-02 23:15:13 -0700

    add script

---
+++ write-test.py
@@ -0,0 +1,21 @@
+from tecombine import CombineArchive
+
+archive = CombineArchive
+
+# Add SBML
+archive.addFile(
+    './model/BIOMD0000000144.xml', # source path
+    './model/BIOMD0000000144.xml', # dest path
+    KnownFormats.lookupFormat('sbml'),
+    True # master attribute
+)
+
+# Add SED-ML
+archive.addFile(
+    './experiment/Calzone2007-simulation-figure-1B.xml', # source path
+    './experiment/Calzone2007-simulation-figure-1B.xml', # dest path
+    KnownFormats.lookupFormat('sedml'),
+    True # master attribute
+)
+
+archive.writeToFile('out.omex');
